050e5b409ffbd1df156707d706fcf59e3fd44af6
middledcoder <middledcoder55@gmail.com>
2020-03-24 00:01:24 +0800

    deleting data

--- select.php
+++ select.php
@@ -11,13 +11,16 @@
      
      foreach($result as $row){
          printf(
-            '<li><span style="color: %s">%s (%s)</span></li>',
+            '<li><span style="color: %s">%s (%s)</span>
+            <a href="delete.php?id=%s">Delete</a>
+            </li>',
             htmlspecialchars($row['color'], ENT_QUOTES),
             htmlspecialchars($row['name'], ENT_QUOTES),
-            htmlspecialchars($row['gender'], ENT_QUOTES));
+            htmlspecialchars($row['gender'], ENT_QUOTES),
+            htmlspecialchars($row['id'], ENT_QUOTES));
      }
 
      $db->close();
 
 ?>
-</ul>
+</ul>   
---
+++ update.php
@@ -0,0 +1,104 @@
+<?php
+// if(isset($_POST['searchterm'])) {
+//     //isset() - for checking
+//     //means isset will return true if there is value 
+//     //it means  $_POST['searchterm'] there is something in that key searchterm
+//     // if there is something insecure !!!!!!
+//     // echo $_POST['searchterm'];
+//     echo htmlspecialchars($_POST['searchterm'], ENT_QUOTES); 
+// // now this practice will not output the code type in search form but will convert into html entities instead <i>Hello World!</i> will now not count as html 
+// }
+$name = '';
+$gender = '';
+$color = '';
+
+
+if(isset($_POST['submit'])) {
+//setting the values of the variable
+    $ok = true;
+    if(!isset($_POST['name']) || $_POST['name'] === ''){
+        $ok = false;
+    }else {
+        $name = $_POST['name']; // do not write with this practice start with isset so that it will check and no problem if no value in the form
+    };
+    if(!isset($_POST['gender']) || $_POST['gender'] === ''){
+        $ok = false;
+    }else{
+        $gender = $_POST['gender'];
+    };
+    if(!isset($_POST['color']) || $_POST['color'] === ''){
+        $ok = false;
+    }else{
+        $color = $_POST['color'];
+    };
+    if($ok) {
+        // add database code here
+        $db = new mysqli( //connecting db
+            'localhost', //database host
+            'root', //database user
+            '', //database password
+            'php'); //database name
+        $sql = sprintf(
+            //inserting the data
+            "INSERT INTO users (name, gender, color) VALUES (
+                '%s','%s','%s')",
+            $db->real_escape_string($name),
+            $db->real_escape_string($gender),$db->real_escape_string($color));
+        $db->query($sql); //send to database
+        // other method for avoid sql injection
+        // $stmt = $db->prepare(
+        //     "INSERT INTO table (name, gender, color) VALUES (?, ?, ?)");
+        // $stmt->bind_param('ss', $name, $gender, $color);
+        // $stmt->execute(); //this is the other way to insert data without sql injection
+                echo '<p>User added.</p>';
+        $db->close(); //close connection
+
+    }
+} 
+
+?>
+<form 
+action="" 
+method="post"> 
+<!-- action=""  where to send the form data-->
+<!-- method=""  how to send the form data-->
+User name: <input type="text" name="name" value="<?php
+    echo htmlspecialchars($name, ENT_QUOTES);
+?>"><br>
+Gender: 
+    <input type="radio" name="gender" value="f"<?php 
+        if($gender === 'f'){
+            echo ' checked';
+        }
+    ?>> Female
+    <input type="radio" name="gender" value="m"<?php 
+        if($gender === 'm'){
+            echo ' checked';
+        }
+    ?>> Male
+    <input type="radio" name="gender" value="o"<?php 
+        if($gender === 'o'){
+            echo ' checked';
+        }
+    ?>> Other <br />
+Favorite color:
+<select name="color">
+    <option value="">Please select</option>
+    <option value="#f00"<?php
+        if($color === '#f00'){
+            echo ' selected';
+        }
+    ?>>Red</option>
+    <option value="#0f0"<?php
+        if($color === '#0f0'){
+            echo ' selected';
+        }
+    ?>>Green</option>
+    <option value="#00f"<?php
+        if($color === '#00f'){
+            echo ' selected';
+        }
+    ?>>Blue</option>
+</select> <br />
+<input type="submit" name="submit" value="Register">
+</form>
